0142f5b922117be5fed5b02ced426f0134348d7b
EhsanTgv <EhsanTgv@gmail.com>
2021-10-07 22:10:56 +0330

    Calling Adapter

--- app/src/main/java/com/taghavi/expandablerecyclerview/MainActivity.kt
+++ app/src/main/java/com/taghavi/expandablerecyclerview/MainActivity.kt
@@ -2,10 +2,42 @@ package com.taghavi.expandablerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.taghavi.expandablerecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding) // or _binding!!
+    private lateinit var expandableAdapter: ExpandableCategoryAdapter
+    private val expandableItems = ArrayList<Item>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        for (i in 1..5) {
+            val parent = Parent("Parent sequence $i")
+            val childItems = arrayListOf<Child>()
+            for (j in 1..5) {
+                childItems.add(Child(parent, j, "Child sequence $j"))
+            }
+            parent.childItems.addAll(childItems)
+            expandableItems.add(parent)
+        }
+        expandableAdapter = ExpandableCategoryAdapter(this, expandableItems)
+        binding.expandableRecycler.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = expandableAdapter
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
